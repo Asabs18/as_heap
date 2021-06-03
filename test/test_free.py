2e3a53234780976469e7a3f6d090f9aeba1aaf29
Asabs18 <aidan.j.sabatini@icloud.com>
2021-06-03 12:56:06 -0400

    Update test_free.py

--- test/test_free.py
+++ test/test_free.py
@@ -1,16 +1,28 @@
 import unittest
 from free import *
 
+definitlyAPointer = []
+
 class TestFree(unittest.TestCase):
 
-    def test_freeReturnsZero(self):
-        #Should Pass
-        self.assertEqual(free(32), 0)
-        self.assertEqual(free(5), 0)
+    def test_freeReturnsNothing(self):
+        self.assertEqual(free(definitlyAPointer), None)
+        self.assertEqual(free(10), None)
+        self.assertEqual(free(True), None)
+        self.assertEqual(free(10.5), None)
 
-        #Should Fail
-        self.assertNotEqual(free(1), 1)
-        self.assertNotEqual(free(31), 32)
+    def test_freeDestroysPointer(self):
+        self.assertEqual(definitlyAPointer, [])
+        free(definitlyAPointer)
+        self.assertEqual(definitlyAPointer, None)
+        definitlyAPointer = []
+
+    def test_freeTwiceStillNull(self):
+        free(definitlyAPointer)
+        self.assertEqual(definitlyAPointer, None)
+        free(definitlyAPointer)
+        self.assertEqual(definitlyAPointer, None)
+        definitlyAPointer = []
 
 if __name__ == '__main__':
     unittest.main()
