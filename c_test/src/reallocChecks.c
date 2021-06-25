bca98892ff0e86fec74d6ed06d125f8fb9005444
Asabs18 <aidan.j.sabatini@icloud.com>
2021-06-25 12:29:37 -0400

    fail unittests

--- c_test/src/reallocChecks.c
+++ c_test/src/reallocChecks.c
@@ -1,14 +1,70 @@
 #include <assert.h>
 #include "munit_ex.h"
 
-//Command Type correctly returns C command 
+#define MAXHEAPSIZE = 1000
+
 static MunitResult
-testTest(const MunitParameter params[], void* data) {
-	return MUNIT_SKIP;
+reallocReturnsNullOnFail1(const MunitParameter params[], void* data){
+	size_t bytes = -1;
+	size_t newBytes = 10;
+
+	int output = realloc(bytes, newBytes);
+
+	munit_assert(output == NULL);
+	return MUNIT_OK;
 }
+
+static MunitResult
+reallocAllocReturnsNullOnFail2(const MunitParameter params[], void* data){
+	size_t bytes = MAXHEAPSIZE + 1;
+	size_t newBytes = 10;
+
+	int output = realloc(bytes, newBytes);
+
+	munit_assert(output == NULL);
+	return MUNIT_OK;
+}
+
+static MunitResult
+reallocAllocReturnsNullOnFail3(const MunitParameter params[], void* data){
+	size_t bytes = 10;
+	size_t newBytes = MAXHEAPSIZE + 1;
+
+	int output = realloc(bytes, newBytes);
+
+	munit_assert(output == NULL);
+	return MUNIT_OK;
+}
+
+static MunitResult
+reallocAllocReturnsNullOnFail4(const MunitParameter params[], void* data){
+	size_t bytes = 0;
+	size_t newBytes = 0;
+
+	int output = realloc(bytes, newBytes);
+
+	munit_assert(output == NULL);
+	return MUNIT_OK;
+}
+
+static MunitResult
+reallocAllocReturnsNullOnFail5(const MunitParameter params[], void* data){
+	size_t bytes = 16;
+	size_t newBytes = 0;
+
+	int output = realloc(bytes, newBytes);
+
+	munit_assert(output == NULL);
+	return MUNIT_OK;
+}
+
 
 //declares the test suite to run each test in this file
 MunitTest reallocCheck_tests[] = {
-	munit_ex_register_test(testTest, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL),
+	munit_ex_register_test(reallocReturnsNullOnFail1, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL),
+	munit_ex_register_test(reallocReturnsNullOnFail2, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL),
+	munit_ex_register_test(reallocReturnsNullOnFail3, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL),
+	munit_ex_register_test(reallocReturnsNullOnFail4, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL),
+	munit_ex_register_test(reallocReturnsNullOnFail5, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL),
 	{ NULL, NULL, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL }
 };
